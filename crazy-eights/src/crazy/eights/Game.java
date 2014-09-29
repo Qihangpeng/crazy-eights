15c31ffb6cd113927b5e49975c33b2a50cec72da
brendon-boldt <brendon_boldt@yahoo.com>
2014-09-29 14:42:27 -0400

    Test commit for NetBeans integration

---
+++ crazy-eights/src/crazy/eights/Game.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crazy.eights;
+
+//Here is a comment line.
+
+/**
+ *
+ * @author brendon
+ */
+public class Game {
+    public static void main(String[] args) {
+        Deck deck = new Deck();
+        deck.deal();
+//        System.out.println(Deck.drawPile.get(index));
+    }
+}
