65dab5a6fa88b7bf1079f70a3d9721926271f24d
Krisztián Köves <kkoves3@gmail.com>
2014-10-06 14:47:07 -0400

    Added shuffle method to Deck class

--- Deck.java
+++ Deck.java
@@ -6,17 +6,22 @@
  */
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Deck {
   ArrayList<Card> cards;
+  Random rand;
   
   public Deck() { 
     cards = new ArrayList<Card>();
+    rand = new Random(0);
     
     for(Card.Rank r : Card.Rank.values()) {
       for(Card.Suit s : Card.Suit.values())
         cards.add(new Card(r,s));
     }
+    
+    shuffle();
   }
   
   @Override
@@ -27,6 +32,16 @@ public class Deck {
       s = s + (i+1) + ". " + cards.get(i) + "\n";
     
     return s;
+  }
+  
+  public void shuffle() {
+    for(int index=0; index < cards.size(); index++) {
+      Card card1 = cards.get(index);
+      int lottery = rand.nextInt(cards.size());
+      Card card2 = cards.get(lottery);
+      cards.set(index,card2);
+      cards.set(lottery,card1);
+    }
   }
   
   public static void main(String[] args) { 
